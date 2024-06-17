58f58ac2fbaece23bb15a9eecdf99caaf3d96501
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2024-06-17 14:09:45 +0200

    add style for card and post-list

--- src/parts/post-list.php
+++ src/parts/post-list.php
@@ -10,18 +10,26 @@ $posts = $request->fetchAll(PDO::FETCH_ASSOC);
 
 <?php foreach ($posts as $post): ?>
 
-<div class="card column" >
-    <div class="infos-match">
-        <h2 class="categorie"><?php echo $post['categorie']; ?></h2>
-        <h3 class="equipe"><?php echo $post['equipe1']; ?></h3>
-        <h3 class="equipe"><?php echo $post['equipe2']; ?></h3>
-        <h3 class="groupe"><?php echo $post['groupe']; ?></h3>     
+<div class="card-row" >
+    <div class="card-left">
+        <div class="title-card">
+            <h1><?php echo $post['equipe1']; ?> vs <?php echo $post['equipe2']; ?></h1>
+            <h4 class="date-time"><?php echo $post['date_heure']; ?></h4>
+        </div>
+        <div class="infos-match">
+            <h4 class="categorie">catégorie : <?php echo $post['categorie']; ?></h4>
+            <h4 class="groupe">groupe : <?php echo $post['groupe']; ?></h4>     
+        </div>
+        <div class="infos-card">
+            <h3 class="lieu"><?php echo $post['lieu']; ?></h3>
+            <h4 class="description"><?php echo $post['description']; ?></h4> 
+        </div> 
     </div>
-    <div class="infos-card">
-        <h2 class="date-time"><?php echo $post['date_heure']; ?></h2>
-        <h3 class="lieu"><?php echo $post['lieu']; ?></h3>
-        <h3 class="prix"><?php echo $post['prix']; ?></h3>
-        <h3 class="description"><?php echo $post['description']; ?></h3> 
-    </div>          
+    <div class="card-right">
+        <div class="price-container">
+        <h3 class="price"><?php echo $post['prix']; ?> €</h3>
+        </div>
+        <button>Encheres</button>
+    </div>
 </div>
 <?php endforeach ?>
